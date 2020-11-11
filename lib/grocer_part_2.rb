f37dcf095862f37e391edcb710d4caaf0f9d4dd6
Jaime David <jaimedavid01@yahoo.com>
2020-11-11 08:44:04 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -34,7 +34,10 @@ def apply_clearance(cart)
   # Consult README for inputs and outputs
   #
   # REMEMBER: This method **should** update cart
-
+counter = 0 
+while counter < cart.length
+if cart[counter][:clearance]
+  cart[counter][:price] = (cart[counter][:price] - (cart[counter][:price]))
 
 
 end
